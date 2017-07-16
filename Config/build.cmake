dead5bb0eefdd96dfe6c92d1f0a70eb5713d52a1
Dami <dami@creabox.net>
2017-07-16 13:48:19 +0200

    Upgrade dependencies and add coverage skel

--- Config/build.cmake
+++ Config/build.cmake
@@ -1,65 +1,24 @@
-# Find Curl
-find_package(CURL REQUIRED)
+# Create test coverage target (gcov)
+if (CMAKE_BUILD_TYPE STREQUAL "Coverage")
+    message("-- Activate Coverage for ${PROJECT_NAME}")
+	set(CMAKE_CXX_OUTPUT_EXTENSION_REPLACE ON)
+	set(CMAKE_CXX_FLAGS "-g -O0 -Wall -fprofile-arcs -ftest-coverage")
+    set(CMAKE_C_FLAGS "-g -O0 -Wall -W -fprofile-arcs -ftest-coverage")
+    set(CMAKE_EXE_LINKER_FLAGS "-fprofile-arcs -ftest-coverage")
 
-# Include sub projects
-find_dependencies(cxx-log)
-find_dependencies(swarm-commons)
-find_dependencies(swarm-mapping)
-find_dependencies(swarm-http-api)
-find_dependencies(swarm-http-server)
-find_dependencies(swarm-http-fcgi)
-
-# Create targets
-add_executable(swarm-http-sample
-    Sources/swarm/http/main.cxx
-    Sources/swarm/http/process/RequestInfoProcess.cxx Sources/swarm/http/process/RequestInfoProcess.hxx
-)
-
-# Properties of targets
-
-# Add definitions for targets
-# Values:
-#   * Debug: -DSWARM_HTTP_SAMPLE_DEBUG=1
-#   * Release: -DSWARM_HTTP_SAMPLE_DEBUG=0
-#   * other: -DSWARM_HTTP_SAMPLE_DEBUG=0
-target_compile_definitions(swarm-http-sample  PUBLIC "SWARM_HTTP_SAMPLE_DEBUG=$<CONFIG:Debug>")
+endif()
 
 # Global includes. Used by all targets
+# Note:
+#   * header location in project: Foo/Source/foo/Bar.hpp
+#   * header can be included by C++ code `#include <foo/Bar.hpp>`
+#   * header location in project: ${CMAKE_CURRENT_BINARY_DIR}/bar_export.hpp
+#   * header can be included by: `#include <bar_export.hpp>`
 target_include_directories(
-    swarm-http-sample
+    ${PROJECT_NAME} 
     
     PUBLIC
-        "$<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/Sources>"
-        "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>"
-        
-    PRIVATE
-        ${cxx-log_INCLUDE_DIR}
-        ${swarm-commons_INCLUDE_DIR}
-        ${swarm-mapping_INCLUDE_DIR}
-        ${swarm-http-api_INCLUDE_DIR}
-        ${swarm-http-server_INCLUDE_DIR}
-        ${swarm-http-fcgi_INCLUDE_DIR}
-)
+    "$<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/Sources>"
+    "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>"
 
-target_link_libraries(
-    swarm-http-sample
-
-    cxx-log
-    swarm-commons
-    swarm-mapping
-    swarm-http-api
-    swarm-http-server
-    swarm-http-fcgi
-)
-
-# Targets:
-#   * <prefix>/lib/${target}.a
-#   * header location after install: <prefix>/include/*.hxx
-#   * headers can be included by C++ code `#include <project/*.hxx>`
-install(
-    TARGETS swarm-http-sample
-    EXPORT "${targets_export_name}"
-    LIBRARY DESTINATION "lib"
-    ARCHIVE DESTINATION "lib"
-    RUNTIME DESTINATION "bin"
 )
